ea2f7c2923266c1c4f82765762b9d61470419801
Galo Guevara <galo.guevara.torres@udla.edu.ec>
2026-06-04 19:59:31 -0500

    create project

---
+++ srp-solid-principles/src/main/java/edu/udla/calidad/srpsolidprinciples/SrpSolidPrinciplesApplication.java
@@ -0,0 +1,13 @@
+package edu.udla.calidad.srpsolidprinciples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrpSolidPrinciplesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SrpSolidPrinciplesApplication.class, args);
+    }
+
+}
---
+++ srp-solid-principles/src/test/java/edu/udla/calidad/srpsolidprinciples/SrpSolidPrinciplesApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.udla.calidad.srpsolidprinciples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrpSolidPrinciplesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
